b84839f1fe4bc96414ff9f9ab3171d8cc47696aa
SHELL TOKYO <74878846+SHELLTOKYO@users.noreply.github.com>
2020-12-04 22:14:10 +0300

    Add files via upload

---
+++ Tokyo.sh
@@ -0,0 +1,243 @@
+#!/bin/bash
+clear
+
+ cd ..
+ rm -rif usr/etc/motd
+ cd $HOME
+
+printf "\033[91m#############<<TOKYO>>#############\n\r"
+printf "\033[92m#        This Tool Crate BY       #\n\r"
+printf "\033[92m#            SHELL TOKYO          #\n\r"
+printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[91m#############<<TOKYO>>#############\n\r"
+sleep 3
+xdg-open https://t.me/Kurdish_Cracker0
+pkg update -y
+clear
+pkg upgrade
+sleep 10
+clear
+printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[91m#   Chawre bka ta command dabzet  #\n\r"
+printf "\033[91m#      Snapchat {shell_tokyo}     #\n\r"
+printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
+sleep 5
+termux-setup-storage
+clear
+sleep 3
+printf "\033[93m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[91m#  chawre bka ta python dabzet    #\n\r"
+printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[93m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+pkg install python -y
+clear
+printf "\033[94m#############<<SHELL TOKYO>>############\n\r"
+printf "\033[92m#  pythonish ba sarkawtuy dawnload bw  #\n\r"
+printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[94m#############<<SHELL TOKYO>>############\n\r"
+clear
+printf "\033[93m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[91m#  chawre bka ta python2 dabzet   #\n\r"
+printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[93m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+pkg install python2 -y
+clear
+printf "\033[94m#############<<SHELL TOKYO>>############\n\r"
+printf "\033[92m#   pythonish ba sarkawtuy dawnload bw #\n\r"
+printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[94m#############<<SHELL TOKYO>>############\n\r"
+clear
+printf "\033[91m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[95m#     chawre bka ta git dabzet    #\n\r"
+printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[91m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+pkg install git -y
+clear
+printf "\033[94m#############<<SHELL TOKYO>>############\n\r"
+printf "\033[92m#    gitish ba sarkawtuy dawnload bw   #\n\r"
+printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[94m#############<<SHELL TOKYO>>############\n\r"
+clear
+printf "\033[91m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[95m#     chawre bka ta pip2 dabzet   #\n\r"
+printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"  
+printf "\033[91m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+pip2 install requests mechanize
+clear
+printf "\033[92m#############<<SHELL TOKYO>>############\n\r"
+printf "\033[95m#   pip2ish ba sarkawtuy dawnload bw   #\n\r"   
+printf "\033[95m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[92m#############<<SHELL TOKYO>>############\n\r"
+clear
+printf "\033[95m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[92m#     chawre bka ta nano dabzet   #\n\r"
+printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[95m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+pkg install nano -y
+clear
+printf "\033[96m#############<<SHELL TOKYO>>############\n\r"
+printf "\033[91m#   nanoish ba sarkawtuy dawnload bw   #\n\r"
+printf "\033[95m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[92m#############<<SHELL TOKYO>>############\n\r"
+clear
+printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[93m#     chawre bka ta ruby dabzet   #\n\r"
+printf "\033[93m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+pkg install ruby-y
+clear
+printf "\033[97m#############<<SHELL TOKYO>>############\n\r"
+printf "\033[92m#   rubyish ba sarkawtuy dawnload bw   #\n\r"
+printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[97m#############<<SHELL TOKYO>>############\n\r"
+clear
+printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[97m#      chawre bka ta php dabzet   #\n\r"
+printf "\033[97m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+pkg install php -y
+clear
+printf "\033[93m#############<<SHELL TOKYO>>############\n\r"
+printf "\033[92m#    phpish ba sarkawtuy dawnload bw   #\n\r"
+printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[93m#############<<SHELL TOKYO>>############\n\r"
+clear
+printf "\033[91m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[94m#      chawre bka ta curl dabzet  #\n\r"
+printf "\033[94m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[91m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+pkg install curl -y
+clear
+printf "\033[92m#############<<SHELL TOKYO>>############\n\r"
+printf "\033[95m#   curlish ba sarkawtuy dawnload bw   #\n\r"
+printf "\033[95m#         Snapchat {shell_tokyo}       #\n\r" 
+printf "\033[92m#############<<SHELL TOKYO>>############\n\r"
+clear
+printf "\033[96m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[91m#     chawre bka ta figlet dabzet #\n\r"
+printf "\033[91m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[96m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+pkg install figlet -y
+clear
+
+printf "\033[96m#############<<SHELL TOKYO>>############\n\r"
+printf "\033[92m#   figletish ba sarkawtuy dawnload bw #\n\r"
+printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[96m#############<<SHELL TOKYO>>############\n\r"
+clear
+figlet TOKYO
+
+printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[92m#       chawre bka ta zip dabzet  #\n\r"
+printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+pkg install zip -y
+clear
+figlet TOKYO
+
+printf "\033[96m#############<<SHELL TOKYO>>############\n\r"
+printf "\033[92m#     zipish ba sarkawtuy dawnload bw  #\n\r"
+printf "\033[92m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[96m#############<<SHELL TOKYO>>############\n\r"
+clear
+figlet TOKYO
+
+printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[92m#     chawre bka ta unzip dabzet  #\n\r"
+printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+pkg install unzip -y 
+clear
+figlet TOKYO
+
+printf "\033[91m#############<<SHELL TOKYO>>############\n\r"
+printf "\033[96m#   unzipish ba sarkawtuy dawnload bw  #\n\r"
+printf "\033[96m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[91m#############<<SHELL TOKYO>>############\n\r"
+clear
+figlet TOKYO
+
+printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[95m#      chawre bka ta fish dabzet  #\n\r"
+printf "\033[95m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
+skeep 4
+pkg install fish -y
+clear
+figlet TOKYO
+
+printf "\033[91m#############<<SHELL TOKYO>>############\n\r"
+printf "\033[96m#   fishish ba sarkawtuy dawnload bw   #\n\r"
+printf "\033[96m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[91m#############<<SHELL TOKYO>>############\n\r"
+clear
+figlet TOKYO
+
+printf "\033[93m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[91m#   chawre bka ta openssl dabzet  #\n\r"
+printf "\033[91m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[93m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+pkg install openssl-y
+clear
+figlet TOKYO
+
+printf "\033[91m#############<<SHELL TOKYO>>############\n\r"
+printf "\033[96m#  opensslish ba sarkawtuy dawnload bw #\n\r"
+printf "\033[96m#         Snapchat {shell_tokyo}       #\n\r"
+printf "\033[91m#############<<SHELL TOKYO>>############\n\r"
+clear
+figlet TOKYO
+
+printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[95m#    Tawaw dlm Joine Channle ba   #\n\r"
+printf "\033[95m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+clear
+figlet TOKYO
+
+printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[92m#    Tawaw dlm Joine Channle ba   #\n\r"
+printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[94m##########<<SHELL TOKYO>>##########\n\r"
+sleep 4
+clear
+
+figlet TOKYO
+
+sleep 7
+printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[91m#        addm bka la snapchat     #\n\r"
+printf "\033[91m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
+clear
+figlet TOKYO
+printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
+printf "\033[91m#        addm bka la snapchat     #\n\r"
+printf "\033[91m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[92m##########<<SHELL TOKYO>>##########\n\r"
+clear
+figlet snapchat
+xdg-open https://www.snapchat.com/add/shell_tokyo
+sleep 10
+clear
+printf "\033[91m#############<<TOKYO>>#############\n\r"
+printf "\033[92m#        This Tool Crate BY       #\n\r"
+printf "\033[92m#            SHELL TOKYO          #\n\r"
+printf "\033[92m#        snapchat {shell_tokyo}   #\n\r"
+printf "\033[91m#############<<TOKYO>>#############\n\r"
+sleep 4
+figlet Bay
+
